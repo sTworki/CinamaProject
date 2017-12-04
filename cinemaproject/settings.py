b14b6f347fe7f81b5ca4b3570b657ab5157e73e9
kiwi <dawidkaczmarski@gmail.com>
2017-12-04 22:27:41 +0000

    Set up database. Modified cinema class.

--- cinemaproject/settings.py
+++ cinemaproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cinemas',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'cinemaproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cinemaprojectdb',
+        'USER': 'stworki',
+        'PASSWORD': '1512',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- cinemas/models.py
+++ cinemas/models.py
@@ -3,6 +3,5 @@ from django.db import models
 
 # Create your models here.
 
-class Cinema:
-    def __init__(self):
-        self.name = name
+class Cinema(models.Model):
+    name = models.CharField(max_length=50)
